bcb7f64247e323e10d3872179d5832e7fb6def51
mononobe <ryoya.goemono@gmail.com>
2022-06-07 21:33:33 +0900

    Rubyからdb操作できることを確認

---
+++ db.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'pg'
+
+# Output a table of current connections to the DB
+conn = PG.connect( dbname: 'sinatra' )
+# conn.exec( "SELECT * FROM todos" ) do |result|
+#   result.each do |tuple|
+#     puts tuple['id']
+#     puts tuple['title']
+#   end
+# end
+
+# conn.exec("insert into todos values (NULL, 'insert_test2', 'piyo');")
+conn.exec("insert into todos (title, body) values ('Kudou', 'Kyoto');")
+
+# conn.exec("delete from todos where title='Kudou';")
+
+conn.exec( "SELECT * FROM todos" ) do |result|
+  result.each do |tuple|
+    puts tuple['id']
+    puts tuple['title']
+  end
+end
